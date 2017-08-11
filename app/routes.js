18bbccb36b728fb8a4c6a43a502733e486367417
Oisin <oisinodonnell@hotmail.com>
2017-08-11 09:40:45 +0100

    Register User begun, active field active to user entity
    List of retailers added to login controller to provide drop down list on register user form.
    
    User entity updated to include the attr 'active'. When a retail manager registers, active = false, the administrator must enable the account belore access is granted to the system. This creates a knock on requirement to check at login.
    Will do this in REST, returning failed login attempt if the use is not active.

---
+++ app/routes.js
@@ -0,0 +1,106 @@
+angular.module('routes', []).config(function ($routeProvider) {
+
+  $routeProvider
+
+    .when('/', {
+      controller: 'LoginController', templateUrl: 'js/login/login.view.html',
+      controllerAs: 'vm', requireLogin: false
+    })
+    .when('/login', {
+      controller: 'LoginController', templateUrl: 'js/login/login.view.html',
+      controllerAs: 'vm', requireLogin: false
+    })
+    .when('/register', {
+      controller: 'LoginController', templateUrl: 'js/login/register.view.html',
+      controllerAs: 'vm', requireLogin: false
+    })
+    .when('/home', {
+      controller: 'HomeController', templateUrl: 'js/login/home.view.html',
+      controllerAs: 'vm', requireLogin: true
+    })
+    .when('/logout', {
+      controller: 'HomeController', templateUrl: 'js/login/home.view.html',
+      controllerAs: 'vm', redirectTo: '/login', requireLogin: true
+    })
+    //  ShoppingCentre
+    .when('/ShoppingCentres/List', {
+      controller: 'ShoppingCentresController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    // Users
+    .when('/Users/List', {controller: 'UsersController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Users/Retailer', {controller: 'UsersController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Users/Approve', {
+      controller: 'UsersController',
+      templateUrl: 'js/Approve.User.view.html',
+      controllerAs: 'ac'
+    })
+    // Retailers
+    .when('/Retailers/List', {controller: 'RetailersController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    // Visits
+    .when('/Visits/List', {controller: 'VisitsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Visits/Retailer', {controller: 'VisitsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Visits/Report', {
+      controller: 'VisitsController',
+      templateUrl: 'js/Report.Visit.view.html',
+      controllerAs: 'ac'
+    })
+    // UserPoints
+    .when('/UserPoints/Retailer', {
+      controller: 'UserPointsController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    // LoyaltyRewards
+    .when('/LoyaltyRewards/Retailer', {
+      controller: 'LoyaltyRewardsController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    .when('/LoyaltyRewards/Approve', {
+      controller: 'LoyaltyRewardsController',
+      templateUrl: 'js/Approve.LoyaltyReward.view.html',
+      controllerAs: 'ac'
+    })
+    // Contents
+    .when('/Contents/List', {controller: 'ContentsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Contents/Retailer', {
+      controller: 'ContentsController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    .when('/Contents/Approve', {
+      controller: 'ContentsController',
+      templateUrl: 'js/Approve.Content.view.html',
+      controllerAs: 'ac'
+    })
+    // BonusCodes
+    .when('/BonusCodes/Retailer', {
+      controller: 'BonusCodesController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    // Locations
+    .when('/Locations/List', {controller: 'LocationsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    // Beacons
+    .when('/Beacons/List', {controller: 'BeaconsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    // Zones
+    .when('/Zones/List', {controller: 'ZonesController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    // Ratings
+    .when('/Ratings/List', {controller: 'RatingsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    .when('/Ratings/Retailer', {controller: 'RatingsController', templateUrl: 'js/List.view.html', controllerAs: 'ac'})
+    // Favourites
+    .when('/Favourites/List', {
+      controller: 'FavouritesController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+    .when('/Favourites/Retailer', {
+      controller: 'FavouritesController',
+      templateUrl: 'js/List.view.html',
+      controllerAs: 'ac'
+    })
+
+    .otherwise({redirectTo: '/login'});
+});
